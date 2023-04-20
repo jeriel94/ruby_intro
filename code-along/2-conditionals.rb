60bfe86db1ee67665449476f7e9eec9091496410
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-04-20 00:21:51 +0000

    2 code along

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,47 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+puts true
+puts false
+
 
 # Boolean Expressions
 
+puts 3 > 2
+puts 3 ==2 # single "=" MAKES it true
+puts 3 != 2
+
 # If Conditional Logic
+if 3>2
+puts "math works"
+end
+
 
 # If/Else Conditional Logic
+pw = "tacos"
+user_entered_pw = "not tacos"
+
+if user_entered_pw == pw
+    puts "welcome"
+else puts "wrong pw"
+end
+
 
 # Elsif Conditional Logic
+home_team = 1
+away_team = 1
+
+if home_team > away_team
+    puts "winner winner chicken dinner"
+
+elsif home_team == away_team
+    puts "boring ass tie"
+else
+    puts "you disappointment"
+end
 
 # Combining Expressions
+# &&
+
+# if temp > 60 && conditon = "sunny"
+# ||
